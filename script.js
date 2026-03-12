ad324f6b6b71db7cf60e5b58e02e7a6b4d1ed9da
keinernarvaez <ckeinercano@gmail.com>
2026-03-12 11:35:03 -0500

    ejercicios practicos html+js

---
+++ script.js
@@ -0,0 +1,128 @@
+
+
+function mostrarMensaje(){
+var boton = document.getElementById("mensaje").innerHTML="hola mundo";
+}
+
+
+
+
+function sumar1(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var suma =  num1 + num2;
+    
+
+ document.getElementById("resultado").innerHTML = suma;
+}
+
+
+document.getElementById("rojo").addEventListener("click",function(){
+    var fondo = document.getElementById("fondo");
+    fondo.style.backgroundColor = "red";
+});
+document.getElementById("verde").addEventListener("click",function(){
+    var fondo = document.getElementById("fondo");
+    fondo.style.backgroundColor = "green";
+});
+document.getElementById("azul").addEventListener("click",function(){
+    var fondo = document.getElementById("fondo");
+    fondo.style.backgroundColor = "blue";
+});
+document.getElementById("blanco").addEventListener("click",function(){
+    var fondo = document.getElementById("fondo");
+    fondo.style.backgroundColor = "white";
+});
+
+
+
+function contador(){
+    var contador = document.getElementById("contador");
+    var resultado = parseInt(contador.innerHTML);
+   
+    
+
+    contador.innerHTML = resultado+1;
+}
+
+
+//lista dinamica1
+function agregar(){
+
+    var tarea = document.getElementById("tarea").value;
+    
+    var elemnto = document.createElement("li");
+    elemnto.textContent = tarea;
+
+    document.getElementById("lista").appendChild(elemnto);
+
+
+}
+
+//lista dinamica 2
+function agregar2(){
+
+    var tarea = document.getElementById("tarea1").value;
+    
+    var elemnto = document.createElement("li");
+    elemnto.textContent = tarea;
+
+    document.getElementById("lista1").appendChild(elemnto);
+
+
+}
+
+function validar(){
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var numero = document.getElementById("numero").value;
+
+    if(nombre == "" || email == "" || numero == ""){
+    
+        document.getElementById("datos").innerHTML = "no puedes dejar los campos en blanco";
+    }
+    else{
+        document.getElementById("datos").innerHTML = "bienvenido "+nombre +
+        " Estos son tus datos "+ email +" tu numero es "+numero;
+        
+    }
+
+}
+
+
+function sumar(){
+ var dato1 = parseInt(document.getElementById("dato1").value);
+ var dato2 = parseInt(document.getElementById("dato2").value);
+ 
+ var solucion = dato1 + dato2;
+console.log(resultado);
+ document.getElementById("result").innerHTML = solucion;
+    
+}
+function restar(){
+ var dato1 = parseInt(document.getElementById("dato1").value);
+ var dato2 = parseInt(document.getElementById("dato2").value);
+ 
+ var solucion = dato1 - dato2;
+ document.getElementById("result").innerHTML = solucion;
+}
+
+function multiplicar(){
+ var dato1 = parseInt(document.getElementById("dato1").value);
+ var dato2 = parseInt(document.getElementById("dato2").value);
+ 
+ var  solucion = dato1 * dato2;
+ document.getElementById("result").innerHTML = solucion;
+}
+function dividir(){
+ var dato1 = parseInt(document.getElementById("dato1").value);
+ var dato2 = parseInt(document.getElementById("dato2").value);
+ 
+  var solucion = dato1 / dato2;
+ document.getElementById("result").innerHTML = solucion;
+}
+   
+   
+
+
